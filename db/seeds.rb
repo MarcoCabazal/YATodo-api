4b0c959a1c53e41ae192ff4ded5d0fb46e7df946
Marco Cabazal <marco.cabazal@chillmill.mobi>
2018-02-15 01:05:40 +0800

    Enabled pg support for heroku.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+Todo.destroy_all
 Category.create(title: "Today")
 Category.create(title: "Upcoming")
 Category.create(title: "Later")
+Todo.create(category: Category.first, title: "Expedite")
+Todo.create(category: Category.second, title: "Procastinate")
+Todo.create(category: Category.last, title: "Do something")
